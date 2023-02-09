24d6349ff83373fb7329011ae18495397847213b
Paulo <dpseutroco@gmail.com>
2023-02-09 08:39:49 -0300

    adicionando SpringSecurity

---
+++ src/main/java/com/api/upload/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.api.upload.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                //----------- files
+                .antMatchers("/api/file/**")
+                .permitAll()
+
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+
+
+}
